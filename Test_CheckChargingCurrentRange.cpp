36603db74dd1f53e35edacf020af137e458c2b4d
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-03-08 13:35:44 +0530

    Update Test_CheckChargingCurrentRange.cpp

--- Test_CheckChargingCurrentRange.cpp
+++ Test_CheckChargingCurrentRange.cpp
@@ -3,8 +3,14 @@
 #include "test/catch.hpp"
 #include "CheckChargingCurrentRange.h"
 
-// Test Instance 1 : before starting the coding : BUILD FAILURE
+// Test Instance 1 : before starting the coding : BUILD PASSED after adding Code
 TEST_CASE("Test Instance 1 : Check range for 2 consecutive current samples") {
   int CurrentSamples[] = {4,5};
+  REQUIRE(CheckChargingCurrentSamplesRange(CurrentSamples) == 1);
+}
+
+// Test Instance 2 : before starting the coding : BUILD FAILURE
+TEST_CASE("Test Instance 2 : Check range for samples not available in sorted format") {
+  int CurrentSamples[] = {4,6,5};
   REQUIRE(CheckChargingCurrentSamplesRange(CurrentSamples) == 1);
 }
