8423612938e24c863868220e43cce89ace7a863f
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-03-08 13:23:57 +0530

    Update CheckChargingCurrentRange.c

--- CheckChargingCurrentRange.c
+++ CheckChargingCurrentRange.c
@@ -2,5 +2,18 @@
 #include "CheckChargingCurrentRange.h"
 
 int CheckChargingCurrentSamplesRange (int *CurrentSamples){
-  return 0;
+  int NumOfCurrentSamples;
+  int NumOfConsecutiveRange=0;
+  int DifferenceBetweenSamples;
+  int LoopIndex;
+  
+  NumOfCurrentSamples = sizeof(CurrentSamples)/sizeof(CurrentSamples[0]);
+  
+  for (LoopIndex = 0; LoopIndex<NumOfCurrentSamples ; LoopIndex++) {
+    DifferenceBetweenSamples = CurrentSamples[LoopIndex + 1] - CurrentSamples[LoopIndex];
+    if((DifferenceBetweenSamples == 0) || (DifferenceBetweenSamples == 1)){
+      NumOfConsecutiveRange ++;
+    }
+  }
+       return NumOfConsecutiveRange;
 }
