45c3d8683131e8ee42a93a04111b43841bd9ee5a
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-03-08 15:51:04 +0530

    Update CheckChargingCurrentRange.c

--- CheckChargingCurrentRange.c
+++ CheckChargingCurrentRange.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include "CheckChargingCurrentRange.h"
 
-int CheckChargingCurrentSamplesRange (int *CurrentSamples, int NumOfCurrentSamples){
-  int NumOfConsecutiveRange=0;
-  int DifferenceBetweenSamples;
-  int LoopIndex;
-  int innerLoopIndex;
-  
-  /* Sort array */
-  int Temp;
+void SortArray(int ChargingCurrentSamplesArray[], int NumOfCurrentSamples){
+  int Temp, LoopIndex, innerLoopIndex;
   for (LoopIndex = 0; LoopIndex<NumOfCurrentSamples ; LoopIndex++) {
     for (innerLoopIndex = (LoopIndex+1) ; innerLoopIndex<NumOfCurrentSamples ; innerLoopIndex++) {
-      if(CurrentSamples[LoopIndex] > CurrentSamples[innerLoopIndex]) {    
-               Temp = CurrentSamples[LoopIndex];    
-               CurrentSamples[LoopIndex] = CurrentSamples[innerLoopIndex];    
-               CurrentSamples[innerLoopIndex] = Temp;   
+      if(ChargingCurrentSamplesArray[LoopIndex] > ChargingCurrentSamplesArray[innerLoopIndex]) {    
+               Temp = ChargingCurrentSamplesArray[LoopIndex];    
+               ChargingCurrentSamplesArray[LoopIndex] = ChargingCurrentSamplesArray[innerLoopIndex];    
+               ChargingCurrentSamplesArray[innerLoopIndex] = Temp;   
       }
     }
   }
+}
+
+int FindConsecutiveSamples(int Sample1,int Sample2)
+{
+  int DifferenceBetweenSamples;
+  int ConsecutiveSample =0;
+  DifferenceBetweenSamples = Sample2 - Sample1;
+  if((DifferenceBetweenSamples == 0) || (DifferenceBetweenSamples == 1))
+  {
+    ConsecutiveSample = 1;
+  }   
+  return ConsecutiveSample;
+}
+
+int CheckChargingCurrentSamplesRange (int *CurrentSamples, int NumOfCurrentSamples){
+  int NumOfConsecutiveRange;
+  int DifferenceBetweenSamples;
+  int LoopIndex;
+  
+  SortArray(CurrentSamples,NumOfSamples);
+  
   for (LoopIndex = 0; LoopIndex< (NumOfCurrentSamples-1) ; LoopIndex++) {
-    DifferenceBetweenSamples = CurrentSamples[LoopIndex + 1] - CurrentSamples[LoopIndex];
-    if((DifferenceBetweenSamples == 0) || (DifferenceBetweenSamples == 1)){
-      NumOfConsecutiveRange ++;
-    }
+    NumOfConsecutiveRange = FindConsecutiveSamples(CurrentSamples[LoopIndex],CurrentSamples[LoopIndex+1]);
   }
-       return NumOfConsecutiveRange+1;
+  return NumOfConsecutiveRange+1;
 }
