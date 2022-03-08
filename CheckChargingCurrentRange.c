184e5af69163215e4147c7664f28009b5ba9b4f7
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-03-08 15:29:28 +0530

    Update CheckChargingCurrentRange.c

--- CheckChargingCurrentRange.c
+++ CheckChargingCurrentRange.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include "CheckChargingCurrentRange.h"
 
-int CheckChargingCurrentSamplesRange (int *CurrentSamples){
-  int NumOfCurrentSamples;
+int CheckChargingCurrentSamplesRange (int *CurrentSamples, int NumOfCurrentSamples){
   int NumOfConsecutiveRange=0;
   int DifferenceBetweenSamples;
   int LoopIndex;
   int innerLoopIndex;
   
-  NumOfCurrentSamples = sizeof(*CurrentSamples)/sizeof(CurrentSamples[0]);
-  printf("Size : %d sizeof(CurrentSamples) : %d  sizeof(CurrentSamples[0] %d \n",NumOfCurrentSamples,sizeof(CurrentSamples),sizeof(CurrentSamples[0]));
   /* Sort array */
   int Temp;
   for (LoopIndex = 0; LoopIndex<NumOfCurrentSamples ; LoopIndex++) {
@@ -19,12 +16,8 @@ int CheckChargingCurrentSamplesRange (int *CurrentSamples){
                Temp = CurrentSamples[LoopIndex];    
                CurrentSamples[LoopIndex] = CurrentSamples[innerLoopIndex];    
                CurrentSamples[innerLoopIndex] = Temp;   
-               printf("Temp %d ,CurrentSamples[LoopIndex] %d ,CurrentSamples[innerLoopIndex] %d \n",Temp,CurrentSamples[LoopIndex],CurrentSamples[innerLoopIndex]);
       }
     }
-  }
-  for (LoopIndex = 0; LoopIndex<NumOfCurrentSamples ; LoopIndex++) {
-    printf("%d element : %d \n",LoopIndex,CurrentSamples[LoopIndex]);
   }
   for (LoopIndex = 0; LoopIndex< (NumOfCurrentSamples-1) ; LoopIndex++) {
     DifferenceBetweenSamples = CurrentSamples[LoopIndex + 1] - CurrentSamples[LoopIndex];
