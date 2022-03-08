c82453eacf09390828ce2994c3957feba554db3d
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-03-08 13:48:33 +0530

    Update CheckChargingCurrentRange.c

--- CheckChargingCurrentRange.c
+++ CheckChargingCurrentRange.c
@@ -19,7 +19,7 @@ int CheckChargingCurrentSamplesRange (int *CurrentSamples){
       }
     }
   }
-  for (LoopIndex = 0; LoopIndex<NumOfCurrentSamples-1 ; LoopIndex++) {
+  for (LoopIndex = 0; LoopIndex< (NumOfCurrentSamples-1) ; LoopIndex++) {
     DifferenceBetweenSamples = CurrentSamples[LoopIndex + 1] - CurrentSamples[LoopIndex];
     if((DifferenceBetweenSamples == 0) || (DifferenceBetweenSamples == 1)){
       NumOfConsecutiveRange ++;
