16492b3a7d3409a9ae5ee0919e8d99ca32e4827b
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-03-08 15:51:48 +0530

    Update CheckChargingCurrentRange.h

--- CheckChargingCurrentRange.h
+++ CheckChargingCurrentRange.h
@@ -1,1 +1,3 @@
 int CheckChargingCurrentSamplesRange (int *CurrentSamples,int NumOfCurrentSamples);
+void SortArray(int ChargingCurrentSamplesArray[], int NumOfCurrentSamples);
+int FindConsecutiveSamples(int Sample1,int Sample2);
