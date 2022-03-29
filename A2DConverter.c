d14dc6ea34f9193aac6c13b6d5897e051200039e
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-03-29 18:49:34 +0530

    Update A2DConverter.c

--- A2DConverter.c
+++ A2DConverter.c
@@ -2,6 +2,28 @@
 #include "A2DConverter.h"
 
 
+void PrintReadingFaultyMessage(void)
+{
+	printf("Readings are faulty\n");
+}
+
+
 bool ConvertAnalogToDigitalAmpere(int *CurrentSamples,int NumOfCurrentSamples){
-  return ALL_SAMPLES_NOT_OK ;
+  bool AreAllSamplesOk = ALL_SAMPLES_OK;
+  int loopIndex;
+  
+  for(loopIndex=0;loopIndex<NumOfCurrentSamples;loopIndex++){
+    if(CurrentSamples[i] > 4094)
+		{
+      AreAllSamplesOk = ALL_SAMPLES_NOT_OK;
+			PrintReadingFaultyMessage();	
+      break;
+		}
+  }
+  if(AreAllSamplesOk == ALL_SAMPLES_OK)
+  {
+    //Implement when all samples are ok
+  }
+  
+  return AreAllSamplesOk ;
 }
