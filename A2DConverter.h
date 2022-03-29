ab33f9515fc387bb74d1b99e8f84c9dd95ae93dd
Anisha Baishya <90901197+AnishaBaishya@users.noreply.github.com>
2022-03-29 20:27:33 +0530

    Update A2DConverter.h

--- A2DConverter.h
+++ A2DConverter.h
@@ -1,8 +1,5 @@
 #define ALL_SAMPLES_OK      1
 #define ALL_SAMPLES_NOT_OK  0
 
-#define A2D_RESOLUTION  12
-#define MAXCURRENTVALUE 10
-
 bool ConvertAnalogToDigitalAmpere(int *CurrentSamplesAnalog,int NumOfCurrentSamples,int *CurrentSamplesDigital);
 void PrintReadingFaultyMessage(void);
